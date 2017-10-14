4ce04965e4d4707334a7ca177281ffbfe6c9a601
Austin <akfite@gmail.com>
2017-10-14 11:49:52 -0400

    Added pct change printout to getquote.py

--- getquote.py
+++ getquote.py
@@ -3,15 +3,14 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 
-# parse args for the desired quote
 args = sys.argv
 
 if len(args) < 2:
-    print("Provide a stock ticker to get the last traded price.")
+    print("Provide a stock ticker symbol to get the last traded price.")
     exit()
 else:
     for stock in args[1:]:
-        # get the webpage html and parse it out
+        # try to load the msn page for the stock
         msn_url = 'http://www.msn.com/en-us/money/indexdetails/'
         page = requests.get(msn_url + stock.upper())
         soup = BeautifulSoup(page.content, 'html.parser')
@@ -21,7 +20,12 @@ else:
         currentvalue = currentvalue.text.replace('\n', '')
 
         if currentvalue:
+            # if currentvalue is found, assume we can find the pctchange
+            pctchange = soup.find("div", {"data-role": "percentchange"})
+            pctchange = pctchange.next
+
             # print the result
-            print(stock.upper() + " is currently trading at " + currentvalue)
+            quote = currentvalue + " (" + pctchange + ")"
+            print(stock.upper() + " is currently trading at " + quote)
         else:
             print("Unable to find a quote for " + stock.upper())
